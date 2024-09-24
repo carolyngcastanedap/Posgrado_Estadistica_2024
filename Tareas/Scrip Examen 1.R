ad3057dcf128927593c3348a7963f7e27aa4ae73
Carolyn Posgrado <lanalina1@hotmail.com>
2024-09-24 13:31:55 -0600

    Actualizar

--- Tareas/Scrip Examen 1.R
+++ Tareas/Scrip Examen 1.R
@@ -10,22 +10,31 @@
 # 6. Sacar media 
 # 7. Varianza
 # 8. Sacar desviación estandar
+
+
 # 9. Restricciones
 # 10. Estadistica descriptiva (Tapply, Summary)
 # 11. Determinar observaciones menores o iguales a 16.9 cm de Diamtero
 # 12. Llamar a la variable como factor
 # 13. Hacer un Boxplot
 # 14. Realizar un histograma
+
+# 15. Estadistica parametrica
 # 15. Establecer hipotesis
-# 16. Prueba de normalidad Shapiro
-# 17. Prueba de homogenidad de varianzas
-# 18. Transformar datos
-# 19. Realizar analisis de variansas B4
-# 20. Pruba de t independientes
-# 21. Prueba t dependientes
-# 22. Prueba de Tukey
-# 23. Realizar plot
-# 24. Correlación
+# 16. Valor de confiabilidad
+# 17. Probabilidad- P-VALUE
+# 18. Tipos de distribuciones
+# 17. Prueba de normalidad Shapiro
+# 18. Prueba de homogenidad de varianzas
+# 19. Transformar datos
+# 20. Realizar analisis de variansas (ANOVA) B4
+# 21. Pruba de t independientes
+# 22. Prueba t dependientes
+# 23. Prueba de Tukey
+# 24. Realizar plot, linea de tendencia central y formula
+# 25. Correlación
+# 26. Regresión lineal
+# 27. Ajustar tabla
 
 
 # 1. Abrir un documento web
@@ -43,6 +52,9 @@ View(conjunto)
 # 3. Abrir archivo en excel, seleccionar archivo en files
 prod <- read.csv("produccion.CSV", header = T)
 View(prod)
+
+# Columna -> Variable
+# Fila -> Datos de colecta
 
 # 4. Ayuda de una funcion
 ?tapply
@@ -62,6 +74,11 @@ sd(conjunto$Altura)
 
 
 # 9.Restricciones
+#igual: ==
+# diferente: !=
+# mayor igual: >=
+# menor igual: <=
+
 ins <- subset(profepa, profepa$Inspección >= mean(profepa$Inspección))
 View(ins)
 
@@ -108,7 +125,7 @@ boxplot(spFH$Diametro ~ spFH$Especie,
         xlab = "Especies",
         ylab = "Diametro",
         col= "green")
-
+# Los boxtplot se componen de datos atipicos, max, cuartil 3, cuartil dos (mediana), cuartil 1, minimo
 
 # 14. Realizar un histograma
 
@@ -118,10 +135,27 @@ hist(conjunto$Altura, xlab = "altura", ylab = "frecuencia", main = "Histograma d
 hist(conjunto$Diametro, xlab = "diametro", ylab = "frecuencia", main = "Histograma de Diametro",
      , col = "cadetblue1")
 
+
+# 15. Estadistica parametrica
+# Metodo que se aplica para una distribución normal, si no se consigue se puede transformar, con log, squrt
+# Tambien se pueden tomar mas datos 
+
+
 # 15. Establecer hipotesis
 #Ho:Posee diferencias del peso del la madera entre Barreta y Gavia
 #H1:No posee diferencias del peso del la madera entre Barreta y Gavia
 
+
+# 16. Valor de confiabilidad
+# alfa= 0.05
+
+
+# 17. Probabilidad- P-VALUE
+#Sirve para saber si se acepta o se niega una hipotesis
+
+# 18. Tipos de distribuciones
+#Normal, campana de Gauss (n)
+#Binomiales (m)
 
 # 16. Prueba de normalidad Shapiro, se acepta por que el valor de P es mayor 0.05 
 
@@ -139,7 +173,10 @@ sitios$dapt <- log(sitios$DAP+1)
 sitios$dapt <- sqrt(sitios$DAP)
 
 
-# 19. Realizar analisis de variansas B4
+# 19. Realizar analisis de variansas (ANOVA) B4
+#se acepta si el valo de P es mayor a 0.05
+# Se utiliza para 3 repeticiones o mas
+
 sit.aov <- aov(sitios$dapt ~ sitios$Paraje)
 summary(sit.aov)
 
@@ -172,15 +209,26 @@ plot(TukeyHSD(sit.aov))
 
 # 23. Realizar plot
 #Dependiente: Edad
+
 plot(edad$DAP, edad$EDAD, pch= 19, col= "darkgreen",
      xlab = "Diametro (cm)",
      ylab = "Edad",
      ylim = c(20,140),
      xlim = c(10,50))
-plot(edad$EDAD, edad$DA, pch= 19)
+
+#Linea de tendencia central
+
+abline(edad.lm)
+
+#Formula
+
+text(20, 120, "y = -8.4 * 2.4 (x)")
 
 
 # 24. Correlación
+
+#En una correlación hay variables dependientes e independientes
+
 #alfa=0.05
 #Ho= sI HAY CORRELACIÓN
 #H1= No hay correlación
@@ -189,3 +237,37 @@ plot(edad$EDAD, edad$DA, pch= 19)
 cor.test(edad$DAP, edad$EDAD)
 
 
+# 25. Regresión lineal
+# y=alfa+ beta* X
+# alfa  (INTERCEPTO DE X ES IGUAL A 0)
+# beta (Incremento que se genera entre X y Y)
+edad.lm <- lm(edad$EDAD ~ edad$DAP)
+edad.lm
+#alfa, intercepto -8.4 beta, 2.4 aumenta
+
+#Para obtener la significancia aplico summary
+summary(edad.lm)
+#Residuales:diferencia entre el valor observado y el valor predicho, debe ser cercano a 0
+#coeficiente: evalua la significancia de beta y alfa, si tiene * para que sea significativo
+#R-squared:Predice el valor de la variable dependiente, en este caso la edad
+#Ocupa tener mas datos para que alfa sea significativo
+
+
+# 26. Ajustar tabla
+
+erupciones$res <- erupciones.lm$residuals
+erupciones$edprim <- erupciones.lm$fitted.values
+erupciones$com.res <- erupciones$eruptions - erupciones$edprim
+
+# 27. Suma de residuales
+sum(erupciones$res)
+
+#Varianza 5270 df- "gl"
+sum(erupciones$res^2)
+sum(erupciones$res^2)/270
+
+# 28.Estimar la edad (prima) para los valores de DAP: 15, 30, 45, 47
+# prima <- Intercep + beta*(valores)
+valores <- c(15,30,45,47)
+prima <- -1.87 + 0.75*(valores)
+prima
